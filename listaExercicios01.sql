94eae743f56ca4dda830e8909441cffb9b4a48e3
yMotaBR <guiguimota2017@gmail.com>
2023-09-12 21:05:24 -0300

    Primeiro commit

---
+++ listaExercicios01.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE aula_db_exemplos;
+USE aula_db_exemplos;
+CREATE TABLE vendas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    produto VARCHAR(255) NOT NULL,
+    receita DECIMAL(10, 2) NOT NULL
+);
+CREATE TABLE autores (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    nascimento DATE
+);
+CREATE TABLE livros (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    autor_id INT,
+    FOREIGN KEY (autor_id) REFERENCES autores(id)
+);
+CREATE TABLE alunos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL
+);
+CREATE TABLE matriculas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    aluno_id INT,
+    curso VARCHAR(255) NOT NULL,
+    FOREIGN KEY (aluno_id) REFERENCES alunos(id)
+);
+INSERT INTO autores (nome, nascimento) VALUES 
+('J.K. Rowling', '1965-07-31'),
+('George Orwell', '1903-06-25'),
+('J.R.R. Tolkien', '1892-01-03'),
+('Jane Austen', '1775-12-16'),
+('Agatha Christie', '1890-09-15'),
+('F. Scott Fitzgerald', '1896-09-24');
+INSERT INTO livros (titulo, autor_id) VALUES
+('Harry Potter e a Pedra Filosofal', 1),
+('Harry Potter e a Câmara Secreta', 1),
+('1984', 2),
+('O Senhor dos Anéis', 3),
+('Orgulho e Preconceito', 4),
+('Morte no Nilo', 5),
+('O Grande Gatsby', 6),
+('O Misterioso Caso de Styles', 5),
+('Razão e Sensibilidade', 4);
+INSERT INTO alunos (nome) VALUES 
+('João'),
+('Maria'),
+('Pedro'),
+('Ana'),
+('Beatriz'),
+('Lucas'),
+('Fernanda'),
+('Eduardo'),
+('Luisa'),
+('Roberto');
+INSERT INTO matriculas (aluno_id, curso) VALUES
+(1, 'Engenharia de Software'),
+(2, 'Medicina'),
+(3, 'Engenharia de Software'),
+(3, 'Arquitetura'),
+(5, 'Física'),
+(6, 'História'),
+(7, 'Psicologia'),
+(8, 'Engenharia Civil'),
+(9, 'Artes'),
+(10, 'Engenharia de Software'),
+(10, 'Economia');
+INSERT INTO vendas (produto, receita) VALUES 
+('Produto A', 5000.00),
+('Produto B', 15000.00),
+('Produto C', 8000.00),
+('Produto A', 6000.00),
+('Produto B', 17000.00),
+('Produto C', 3000.00),
+('Produto D', 10000.00),
+('Produto E', 6500.00),
+('Produto F', 8500.00),
+('Produto A', 7000.00),
+('Produto B', 12000.00),
+('Produto E', 4000.00),
+('Produto C', 11000.00),
+('Produto D', 5000.00);
