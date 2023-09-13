c2a4fab435bbda597bdc11359d74183530115ee8
yMotaBR <guiguimota2017@gmail.com>
2023-09-12 21:10:49 -0300

    Quarto exercício

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -83,9 +83,16 @@ INSERT INTO vendas (produto, receita) VALUES
 ('Produto D', 5000.00);
 
 select titulo from livros;
+
 select NOME from autores
 WHERE nascimento < '1900-01-01';
+
 SELECT titulo 
 from livros 
 inner join autores on autor_id=autores.id
 where NOME = 'J.K. Rowling';
+
+select alunos.nome
+from alunos 
+join matriculas on alunos.id = matriculas.aluno_id
+where matriculas.curso = 'Engenharia de Software';
