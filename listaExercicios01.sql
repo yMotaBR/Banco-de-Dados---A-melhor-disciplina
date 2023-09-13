6aa5fd243649217f30a7e90a0d4d679a03fd94e7
yMotaBR <guiguimota2017@gmail.com>
2023-09-12 21:16:57 -0300

    nono exercicio

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -114,3 +114,7 @@ SELECT produto, AVG(receita) AS media_receita
 FROM vendas
 GROUP BY produto;
 
+SELECT produto, SUM(receita) AS receita_total
+FROM vendas
+GROUP BY produto
+HAVING SUM(receita) > 10000;
