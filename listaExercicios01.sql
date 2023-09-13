74c3fd46a993c0d55d4b97b8f2d87a0873576c9f
yMotaBR <guiguimota2017@gmail.com>
2023-09-12 21:24:34 -0300

    vigesimo exercicio

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -160,3 +160,9 @@ SELECT alunos.nome, COUNT(matriculas.id) AS numero_de_matriculas
 FROM alunos
 LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
 GROUP BY alunos.id;
+
+SELECT produto, COUNT() AS quantidade_de_vendas
+FROM vendas
+GROUP BY produto
+ORDER BY quantidade_de_vendas DESC
+LIMIT 1;
