8b253929e61af1af77388ae349b2569deb80d868
yMotaBR <guiguimota2017@gmail.com>
2023-09-12 21:09:50 -0300

    Terceiro exercicio

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -85,3 +85,7 @@ INSERT INTO vendas (produto, receita) VALUES
 select titulo from livros;
 select NOME from autores
 WHERE nascimento < '1900-01-01';
+SELECT titulo 
+from livros 
+inner join autores on autor_id=autores.id
+where NOME = 'J.K. Rowling';
