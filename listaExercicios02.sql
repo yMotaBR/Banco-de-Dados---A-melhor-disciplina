6a7ee3aa24ac5a77414afe03c5447c6260b4d6a1
yMotaBR <guiguimota2017@gmail.com>
2023-09-26 21:08:28 -0300

    Segundo exercicio

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -2,3 +2,8 @@ CREATE PROCEDURE sp_ListarAutores()
 BEGIN
     SELECT * FROM autores;
 END;
+
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria VARCHAR(255))
+BEGIN
+    SELECT * FROM livros WHERE categoria = categoria;
+END;
