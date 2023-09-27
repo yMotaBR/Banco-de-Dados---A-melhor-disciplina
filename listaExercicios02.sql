98ed13d3af5232d5c76ce4c8fee997af91609fc2
yMotaBR <guiguimota2017@gmail.com>
2023-09-26 21:08:51 -0300

    terceiro exercicio

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -7,3 +7,8 @@ CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria VARCHAR(255))
 BEGIN
     SELECT * FROM livros WHERE categoria = categoria;
 END;
+
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria VARCHAR(255), OUT total INT)
+BEGIN
+    SELECT COUNT(*) INTO total FROM livros WHERE categoria = categoria;
+END;
