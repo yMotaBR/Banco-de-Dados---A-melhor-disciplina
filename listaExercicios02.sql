aa01a77dc88fa7b8bf435a87273bc638b58420ec
yMotaBR <guiguimota2017@gmail.com>
2023-09-26 21:53:26 -0300

    nono exercicio

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -63,3 +63,27 @@ CREATE PROCEDURE sp_AutorMaisAntigo(OUT nomeAutor VARCHAR(255))
 BEGIN
     SELECT nome INTO nomeAutor FROM Autor ORDER BY data_nascimento LIMIT 1;
 END;
+
+-- Stored procedure para verificar se há livros em uma determinada categoria
+-- Parâmetros:
+-- categoria: A categoria para verificar livros
+-- possuiLivros: Parâmetro de saída para indicar se há livros na categoria
+
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoria VARCHAR(255), OUT possuiLivros BOOLEAN)
+BEGIN
+    -- Declarando uma variável para armazenar a contagem de livros na categoria
+    DECLARE count INT;
+    
+    -- Contando o número de livros em uma determinada categoria
+    SELECT COUNT(*) INTO count FROM livros WHERE categoria = categoria;
+    
+    -- Verificando se há livros na categoria
+    IF count > 0 THEN
+        -- Definindo o parâmetro de saída como verdadeiro se houver livros
+        SET possuiLivros = TRUE;
+    ELSE
+        -- Definindo o parâmetro de saída como falso se não houver livros
+        SET possuiLivros = FALSE;
+    END IF;
+    
+END;
